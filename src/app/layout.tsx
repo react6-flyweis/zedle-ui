d9f4b2511a43245811324930e153921f9116c6a9
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-19 19:19:20 +0530

    fix: adjust body class for consistent background styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-accent `}
       >
         <Header />
-        <main className="bg-accent">{children}</main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
