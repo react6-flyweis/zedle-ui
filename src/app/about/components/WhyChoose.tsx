5d6a986313335953b214feaa12f894ba11962c0e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-20 16:47:09 +0530

    feat: add WhyChoose component to About page

---
+++ src/app/about/components/WhyChoose.tsx
@@ -0,0 +1,110 @@
+export function WhyChoose() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-8 text-gray-800">
+        Why Choose Zedle
+      </h1>
+
+      <div className="space-y-6">
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            1.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            2.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            3.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            4.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            5.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            6.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <div className="text-lg font-semibold text-gray-800 min-w-[24px]">
+            7.
+          </div>
+          <div>
+            <span className="font-semibold text-gray-800">Lorem Ipsum – </span>
+            <span className="text-gray-700">
+              Lorem ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,12 @@
 import HeroGrid from "./components/HeroGrid";
+import { WhyChoose } from "./components/WhyChoose";
 
 export default function AboutPage() {
   return (
     <div className="flex flex-col">
       <HeroGrid />
+      <WhyChoose />
+
     </div>
   );
 }
