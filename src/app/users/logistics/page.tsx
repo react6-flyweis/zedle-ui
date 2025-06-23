bc2aabb25a8b214eef6f9bdaa921ab9740826ced
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-23 19:29:45 +0530

    feat: add logistics page

---
+++ src/app/users/logistics/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import personalisedLogisticsImage from "@/assets/images/personalised-logistics.png";
+import { AdCarousel } from "@/components/ad-carousel";
+import { Cities } from "@/components/cities";
+import { Hero } from "@/components/hero";
+import { LogisticsBooking } from "@/components/logistics-booking";
+import { Testimonials } from "@/components/testimonials";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <LogisticsBooking />
+      <Cities />
+      <AdCarousel />
+      <Testimonials />
+      <div className="px-10">
+        <Image
+          src={personalisedLogisticsImage}
+          alt="Personalised Recommendations"
+          className="w-full h-auto object-cover"
+        />
+      </div>
+    </div>
+  );
+}
