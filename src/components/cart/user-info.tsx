2ea7c3e841e576e4bd30d1f17397e3f2d37ba347
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-23 14:21:01 +0530

    feat: add UserInfo component for displaying user information

---
+++ src/components/cart/user-info.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserIcon } from "lucide-react";
+
+interface UserInfoProps {
+  name: string;
+  phone: string;
+}
+
+export default function UserInfo({ name, phone }: UserInfoProps) {
+  return (
+    <Card className="rounded-none shadow-none gap-0">
+      <CardHeader className="relative">
+        <div className="absolute size-12 bg-black -left-10 top-0 flex items-center justify-center">
+          <UserIcon className="size-6 text-white" />
+        </div>
+      </CardHeader>
+      <CardContent className="">
+        <h3 className="font-semibold text-lg">Logged in</h3>
+        <p className="text-accent-foreground">
+          {name} | {phone}
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
