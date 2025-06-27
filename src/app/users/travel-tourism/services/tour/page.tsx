4e14a220d7c5cfde6c517161ed357ce9602a6649
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-27 13:52:09 +0530

    feat(travel-tourism): add tour service page

---
+++ src/app/users/travel-tourism/services/tour/page.tsx
@@ -0,0 +1,10 @@
+import { ServicesHero } from "../components/ServicesHero";
+
+export default function page() {
+  return (
+    <div>
+      <ServicesHero />
+      {/* Tour Booking & Reservations Section */}
+    </div>
+  );
+}
