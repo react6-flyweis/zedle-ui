61205f2ebd42b86b697c8f79acaab4c4b99e340c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-30 16:43:37 +0530

    feat(food): include FoodDeliveryPersonalized component in the page

--- src/app/users/food/page.tsx
+++ src/app/users/food/page.tsx
@@ -1,11 +1,13 @@
 import { CategoriesTray } from "./components/CategoriesTray";
 import { FoodDeliveryHero } from "./components/FoodDeliveryHero";
+import { FoodDeliveryPersonalized } from "./components/FoodDeliveryPersonalized";
 
 export default function page() {
   return (
     <div>
       <FoodDeliveryHero />
       <CategoriesTray />
+      <FoodDeliveryPersonalized />
     </div>
   );
 }
