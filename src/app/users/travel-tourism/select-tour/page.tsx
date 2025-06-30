6567d9ba6096d612584304ce69610e695d005014
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-06-30 14:22:04 +0530

    feat(travel-tourism): simplify TourBooking component by removing props

--- src/app/users/travel-tourism/select-tour/page.tsx
+++ src/app/users/travel-tourism/select-tour/page.tsx
@@ -1,29 +1,11 @@
-"use client";
-import { useState } from "react";
-import { useUserLocation } from "@/hooks/useUserLocation";
 import { TourBooking } from "../components/TourBooking";
 import { TravelTourismPersonalised } from "../components/TravelTourismPersonalised";
 
 export default function Page() {
-  const { location } = useUserLocation();
-  const [pickupPoint, setPickupPoint] = useState("");
-  const [dropoffPoint, setDropoffPoint] = useState("");
-  const [activeField, setActiveField] = useState<"pickup" | "dropoff" | null>(
-    null,
-  );
-
   return (
     <div className="">
       <div className="h-[90vh] relative">
-        <TourBooking
-          pickupPoint={pickupPoint}
-          dropoffPoint={dropoffPoint}
-          onPickupChange={setPickupPoint}
-          onDropoffChange={setDropoffPoint}
-          activeField={activeField}
-          setActiveField={setActiveField}
-          userLocation={location}
-        />
+        <TourBooking />
       </div>
       <TravelTourismPersonalised />
     </div>
