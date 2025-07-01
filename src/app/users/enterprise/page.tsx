7d8ea0988f8502cbfc3d5e7c78288813ea867323
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-01 11:14:42 +0530

    feat(enterprise-hub): add page component with EnterpriseHero and OurServicesTray

---
+++ src/app/users/enterprise/page.tsx
@@ -0,0 +1,11 @@
+import { EnterpriseHero } from "./components/EnterpriseHero";
+import { OurServicesTray } from "./components/OurServicesTray";
+
+export default function page() {
+  return (
+    <div>
+      <EnterpriseHero />
+      <OurServicesTray />
+    </div>
+  );
+}
