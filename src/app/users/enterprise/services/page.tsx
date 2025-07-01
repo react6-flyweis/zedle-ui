0e7986182467a27a8d255590c2d0a90e6bcdd1bf
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-01 12:48:15 +0530

    feat(enterprise-hub): add services page with hero

---
+++ src/app/users/enterprise/services/page.tsx
@@ -0,0 +1,11 @@
+import { EnterpriseServicesHero } from "../components/EnterpriseServicesHero";
+
+function page() {
+  return (
+    <div>
+      <EnterpriseServicesHero />
+    </div>
+  );
+}
+
+export default page;
