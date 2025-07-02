2ad770c6b6a6ccb3335237ab280fc09abdd6387e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-02 15:17:15 +0530

    feat(delivery): add navigation  for delivery partner

--- src/app/delivery-partner/layout.tsx
+++ src/app/delivery-partner/layout.tsx
@@ -1,6 +1,25 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import type { NavigationItem } from "@/components/nav-menu";
 import { DeliveryPartnerPersonalized } from "./components/DeliveryPartnerPersonalized";
+
+const deliveryPartnerNavigation: NavigationItem[] = [
+  {
+    title: "Home",
+    href: "/delivery-partner",
+    type: "link",
+  },
+  {
+    title: "Orders",
+    href: "/delivery-partner/orders",
+    type: "link",
+  },
+  {
+    title: "Wallet",
+    href: "/delivery-partner/wallet",
+    type: "link",
+  },
+];
 
 export default function DeliveryPartnerLayout({
   children,
@@ -9,7 +28,7 @@ export default function DeliveryPartnerLayout({
 }) {
   return (
     <>
-      <Header />
+      <Header navigation={deliveryPartnerNavigation} />
       <main>{children}</main>
       <DeliveryPartnerPersonalized />
       <Footer />
