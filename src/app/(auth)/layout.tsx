1f1bd3fc197522fac08152974f89db03abaef7d5
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-02 12:43:23 +0530

    feat(auth): restructure layout to include header and footer

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import { AuthCarousel } from "./components/auth-carousel";
 
 export default function AuthLayout({
@@ -6,16 +8,22 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-[90vh] grid md:grid-cols-2 lg:grid-cols-5">
-      {/* Left side - Image and promotional content */}
-      <div className="relative lg:col-span-2">
-        <AuthCarousel />
-      </div>
+    <div>
+      <Header centeredNavigation />
+      <main>
+        <div className="min-h-[85vh] grid md:grid-cols-2 lg:grid-cols-5">
+          {/* Left side - Image and promotional content */}
+          <div className="relative lg:col-span-2">
+            <AuthCarousel />
+          </div>
 
-      {/* Right side - Auth forms */}
-      <div className="lg:col-span-3 w-full flex items-center justify-center p-8">
-        <div className="w-full max-w-xl">{children}</div>
-      </div>
+          {/* Right side - Auth forms */}
+          <div className="lg:col-span-3 w-full flex items-center justify-center p-8">
+            <div className="w-full max-w-xl">{children}</div>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
