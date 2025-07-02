2714faad178dd5906b0686ef44b6e55cb93304ae
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-02 12:44:59 +0530

    feat(i18n): add localization support for OTP verification and restructure common messages

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,40 @@
+import { headers } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
+  const allHeaders = headers();
+  const url = (await allHeaders).get("referer");
+  const pathname = url ? new URL(url).pathname : "/";
+
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
   const locale = "en";
+  const messages = [];
+
+  // Load the default messages for the locale
+  const defaultMessages = (await import(`./locales/${locale}/common.json`))
+    .default;
+  messages.push(defaultMessages);
+
+  if (pathname.startsWith("/user")) {
+    // Use Next.js server context to determine which messages to load
+    const userMessages = (await import(`./locales/${locale}.json`)).default;
+    messages.push(userMessages);
+    // } else if (pathname.startsWith("/vendor")) {
+    //   const vendorMessages = (await import(`./locales/${locale}/vendor.json`))
+    //     .default;
+    //   messages.push(vendorMessages);
+  } else {
+    const authMessages = (await import(`./locales/${locale}/auth.json`))
+      .default;
+    messages.push(authMessages);
+  }
+
+  // Merge all message objects into a single object
+  const mergedMessages = Object.assign({}, ...messages);
 
   return {
     locale,
-    messages: (await import(`./locales/${locale}.json`)).default,
+    messages: mergedMessages,
   };
 });
