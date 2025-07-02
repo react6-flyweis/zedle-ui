5b71b50484ec3e435f2e60c557647c043042aa8e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-02 17:23:50 +0530

    refactor(i18n): disabled pathname logic for dynamic translation loading

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,10 @@
-import { headers } from "next/headers";
+// import { headers } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
-  const allHeaders = headers();
-  const url = (await allHeaders).get("referer");
-  const pathname = url ? new URL(url).pathname : "/";
+  // const allHeaders = headers();
+  // const url = (await allHeaders).get("referer");
+  // const pathname = new URL(url || "").pathname; // not reliable
 
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
@@ -15,24 +15,30 @@ export default getRequestConfig(async () => {
   const defaultMessages = (await import(`./locales/${locale}/common.json`))
     .default;
   messages.push(defaultMessages);
+  const partnerMessages = (await import(`./locales/${locale}/delivery.json`))
+    .default;
+  const userMessages = (await import(`./locales/${locale}.json`)).default;
 
-  if (pathname.startsWith("/user")) {
-    // Use Next.js server context to determine which messages to load
-    const userMessages = (await import(`./locales/${locale}.json`)).default;
-    messages.push(userMessages);
-  } else if (pathname.startsWith("/delivery-partner")) {
-    const partnerMessages = (await import(`./locales/${locale}/delivery.json`))
-      .default;
-    messages.push(partnerMessages);
-    // } else if (pathname.startsWith("/vendor")) {
-    //   const vendorMessages = (await import(`./locales/${locale}/vendor.json`))
-    //     .default;
-    //   messages.push(vendorMessages);
-  } else {
-    const authMessages = (await import(`./locales/${locale}/auth.json`))
-      .default;
-    messages.push(authMessages);
-  }
+  const authMessages = (await import(`./locales/${locale}/auth.json`)).default;
+  messages.push(partnerMessages, userMessages, authMessages);
+
+  // if (pathname.startsWith("/user")) {
+  //   // Use Next.js server context to determine which messages to load
+  //   const userMessages = (await import(`./locales/${locale}.json`)).default;
+  //   messages.push(userMessages);
+  // } else if (pathname.startsWith("/delivery-partner")) {
+  //   const partnerMessages = (await import(`./locales/${locale}/delivery.json`))
+  //     .default;
+  //   messages.push(partnerMessages);
+  //   // } else if (pathname.startsWith("/vendor")) {
+  //   //   const vendorMessages = (await import(`./locales/${locale}/vendor.json`))
+  //   //     .default;
+  //   //   messages.push(vendorMessages);
+  // } else {
+  //   const authMessages = (await import(`./locales/${locale}/auth.json`))
+  //     .default;
+  //   messages.push(authMessages);
+  // }
 
   // Merge all message objects into a single object
   const mergedMessages = Object.assign({}, ...messages);
