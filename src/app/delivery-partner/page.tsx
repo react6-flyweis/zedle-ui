b4ab2deed23084ac28b52834e54f601d30b543c9
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-02 13:54:37 +0530

    feat(delivery): add page component to render LocatorMap

---
+++ src/app/delivery-partner/page.tsx
@@ -0,0 +1,11 @@
+import { LocatorMap } from "./components/LocatorMap";
+
+export default function page() {
+  return (
+    <div>
+      <div className="h-[85vh] w-full">
+        <LocatorMap />
+      </div>
+    </div>
+  );
+}
