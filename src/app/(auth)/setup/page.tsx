a6fe25d55232536febbe5554b3e052fc7a08b944
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-07 12:57:54 +0530

    feat(vendor:food): enhance vendor signup page to handle multiple categories

--- src/app/(auth)/setup/page.tsx
+++ src/app/(auth)/setup/page.tsx
@@ -1,5 +1,31 @@
+import { VendorFoodSignup } from "../components/VendorFoodSignup";
 import VendorGrocerySignup from "../components/VendorGrocerySignup";
 
-export default function page() {
-  return <VendorGrocerySignup />;
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+  if (!params || !params.type) {
+    return null;
+  }
+  const type = params.type;
+  const category = params.category;
+
+  // if (type === "delivery") {
+  //   return < />;
+  // }
+
+  if (type === "vendor") {
+    switch (category) {
+      case "Grocery":
+        return <VendorGrocerySignup />;
+      case "Food Delivery":
+        return <VendorFoodSignup />;
+      // Add more categories as needed
+      default:
+        return <div>Unsupported vendor category: {category}</div>;
+    }
+  }
 }
