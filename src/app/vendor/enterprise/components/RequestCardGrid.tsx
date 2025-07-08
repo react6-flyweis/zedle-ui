b452ebc2465788b12c7ae6a32690e842619b8d5e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-08 21:29:47 +0530

    feat(vendor:enterprise): update mockRequests in RequestCardGrid to include phone, email, and requestId fields

--- src/app/vendor/enterprise/components/RequestCardGrid.tsx
+++ src/app/vendor/enterprise/components/RequestCardGrid.tsx
@@ -1,10 +1,13 @@
-import { RequestCard } from "./RequestCard";
+import { type IRequest, RequestCard } from "./RequestCard";
 
-const mockRequests = [
+const mockRequests: IRequest[] = [
   {
     name: "Chance Septimus",
+    phone: "+1 9876543210",
+    email: "chanceseptimus@gmail.com",
+    requestId: "1669297452",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -12,8 +15,11 @@ const mockRequests = [
   },
   {
     name: "Gretchen Levin",
+    phone: "+1 123-456-7891",
+    email: "gretchen.levin@example.com",
+    requestId: "1669297453",
     service: "Women's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -21,8 +27,11 @@ const mockRequests = [
   },
   {
     name: "Marilyn Lipshutz",
+    phone: "+1 123-456-7892",
+    email: "marilyn.lipshutz@example.com",
+    requestId: "1669297454",
     service: "Women's Haircut, Long Hair",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -30,8 +39,11 @@ const mockRequests = [
   },
   {
     name: "Livia Curtis",
+    phone: "+1 123-456-7893",
+    email: "livia.curtis@example.com",
+    requestId: "1669297455",
     service: "Women's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -39,8 +51,11 @@ const mockRequests = [
   },
   {
     name: "James Septimus",
+    phone: "+1 123-456-7894",
+    email: "james.septimus@example.com",
+    requestId: "1669297456",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -48,8 +63,11 @@ const mockRequests = [
   },
   {
     name: "Charlie Saris",
+    phone: "+1 123-456-7895",
+    email: "charlie.saris@example.com",
+    requestId: "1669297457",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -57,8 +75,11 @@ const mockRequests = [
   },
   {
     name: "Roger Westervelt",
+    phone: "+1 123-456-7896",
+    email: "roger.westervelt@example.com",
+    requestId: "1669297458",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -66,8 +87,11 @@ const mockRequests = [
   },
   {
     name: "Chance Dokidis",
+    phone: "+1 123-456-7897",
+    email: "chance.dokidis@example.com",
+    requestId: "1669297459",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -75,8 +99,11 @@ const mockRequests = [
   },
   {
     name: "Martin Ekstrom Bothman",
+    phone: "+1 123-456-7898",
+    email: "martin.bothman@example.com",
+    requestId: "1669297460",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -84,8 +111,11 @@ const mockRequests = [
   },
   {
     name: "Nolan Bator",
+    phone: "+1 123-456-7899",
+    email: "nolan.bator@example.com",
+    requestId: "1669297461",
     service: "Men's Haircut",
-    date: "08-May, 2023 Mon",
+    date: "08-May, 2025 Mon",
     time: "16:30-17:00",
     status: "SP Assigned",
     imageUrl:
@@ -97,7 +127,7 @@ export const RequestCardGrid: React.FC = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2  gap-6">
       {mockRequests.map((req) => (
-        <RequestCard key={req.name} {...req} />
+        <RequestCard key={req.name} request={req} />
       ))}
     </div>
   );
