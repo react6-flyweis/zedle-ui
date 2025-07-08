26f867a94de2dbd3e3833f80fed83cba9e523f0e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-08 15:13:59 +0530

    feat(vendor:enterprise): add enterprise navigation

--- src/app/vendor/layout.tsx
+++ src/app/vendor/layout.tsx
@@ -85,29 +85,28 @@ const foodNavigation: NavigationItem[] = [
 //   },
 // ];
 
-// const enterpriseNavigation: NavigationItem[] = [
-//   {
-//     title: "Home",
-//     href: "/vendor/enterprise",
-//     type: "link",
-//   },
-//   {
-//     title: "Search",
-//     href: "/vendor/enterprise/search",
-//     type: "link",
-//     icon: <SearchIcon />,
-//   },
-//   {
-//     title: "Services",
-//     href: "/vendor/enterprise/services",
-//     type: "link",
-//   },
-//   {
-//     title: "Favorites",
-//     href: "/vendor/enterprise/favorites",
-//     type: "link",
-//   },
-// ];
+const enterpriseNavigation: NavigationItem[] = [
+  {
+    title: "Home",
+    href: "/vendor/enterprise",
+    type: "link",
+  },
+  {
+    title: "Clients",
+    href: "/vendor/enterprise/clients",
+    type: "link",
+  },
+  {
+    title: "Staffs",
+    href: "/vendor/enterprise/staffs",
+    type: "link",
+  },
+  {
+    title: "Checkout",
+    href: "/vendor/enterprise/checkout",
+    type: "link",
+  },
+];
 
 function getNavigationForPath(pathname: string): NavigationItem[] {
   if (pathname.startsWith("/vendor/grocery")) {
@@ -120,11 +119,11 @@ function getNavigationForPath(pathname: string): NavigationItem[] {
     return foodNavigation;
   }
   //   if (pathname.startsWith("/vendor/travel-tourism")) {
-  //     return [...travelNavigation];
+  //     return travelNavigation;
   //   }
-  //   if (pathname.startsWith("/vendor/enterprise")) {
-  //     return [...enterpriseNavigation];
-  //   }
+  if (pathname.startsWith("/vendor/enterprise")) {
+    return enterpriseNavigation;
+  }
 
   // Default to grocery navigation
   return groceryNavigation;
