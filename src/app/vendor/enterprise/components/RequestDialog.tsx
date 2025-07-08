8bae0b4402b5d857e0a2f396cfcc1703c5519b12
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-08 21:27:31 +0530

    feat(vendor:enterprise): add RequestDialog component with form and localization support

---
+++ src/app/vendor/enterprise/components/RequestDialog.tsx
@@ -0,0 +1,236 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import type { PropsWithChildren } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import type { IRequest } from "./RequestCard";
+
+export function RequestDialog({
+  request,
+  children,
+}: PropsWithChildren<{ request: IRequest }>) {
+  const { name, email, phone, imageUrl, service, date, time, requestId } =
+    request;
+  const t = useTranslations("requestDialog");
+
+  const formSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    phone: z.string(),
+    service: z.string(),
+    date: z.string(),
+    time: z.string(),
+    requestId: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name,
+      email,
+      phone,
+      service,
+      date,
+      time,
+      requestId,
+    },
+    mode: "onChange",
+  });
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-3xl max-h-[80vh] rounded-2xl p-0 overflow-y-auto">
+        <Form {...form}>
+          <form className="px-6 pb-8 pt-6" autoComplete="off">
+            <div className="font-bold text-base mb-4 text-black">
+              {t("customerDetails")}
+            </div>
+            <div className="flex gap-8 items-start">
+              <div className="relative size-56 rounded-2xl overflow-hidden bg-gray-200 flex-shrink-0 border border-gray-300">
+                <Image
+                  src={imageUrl}
+                  alt={name}
+                  className="object-cover h-full w-full"
+                  height={400}
+                  width={400}
+                  priority
+                />
+              </div>
+              <div className="space-y-3 flex-1">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("name")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("email")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("phone")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="mt-5">
+              <div className="font-bold text-base mb-4 text-black">
+                {t("serviceDetails")}
+              </div>
+
+              <div className="space-y-3">
+                <FormField
+                  control={form.control}
+                  name="service"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("service")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="date"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("date")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="time"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("time")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="requestId"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-sm font-medium text-black">
+                        {t("requestId")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-accent w-full"
+                          {...field}
+                          readOnly
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+            <div className="flex gap-4 mt-10">
+              <Button
+                type="button"
+                variant="outline"
+                className="flex-1 bg-gray-200 text-black font-semibold hover:bg-gray-300"
+              >
+                {t("requestReschedule")}
+              </Button>
+              <Button
+                type="button"
+                className="flex-1 bg-black text-white font-semibold hover:bg-black/90"
+              >
+                {t("accept")}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
