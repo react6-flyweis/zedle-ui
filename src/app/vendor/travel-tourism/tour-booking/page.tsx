b3f57afe5e47b1e70a9e86c175eef0f4f6c072b8
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-10 20:05:06 +0530

    feat(vendor:travel-tourism): add TourRequestCardGrid component and integrate it into the booking page

---
+++ src/app/vendor/travel-tourism/tour-booking/page.tsx
@@ -0,0 +1,17 @@
+import { Testimonials } from "@/components/testimonials";
+import { TourRequestCardGrid } from "./components/TourRequestCardGrid";
+import { VendorTourBookingHero } from "./components/VendorHotelLodgeHero";
+import { VendorTourBookingPersonalized } from "./components/VendorTourBookingPersonalized";
+
+export default function page() {
+  return (
+    <div>
+      <VendorTourBookingHero />
+      <div className="max-w-6xl mx-auto">
+        <TourRequestCardGrid />
+      </div>
+      <Testimonials />
+      <VendorTourBookingPersonalized />
+    </div>
+  );
+}
