d0a52b22e410c5f38aa92f37b27eb69411742b68
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-11 16:08:50 +0530

    feat(vendor:travel-tourism): add chat page with MessageScreen component

---
+++ src/app/vendor/travel-tourism/tour-booking/chat/page.tsx
@@ -0,0 +1,9 @@
+import { MessageScreen } from "@/components/message/MessageScreen";
+
+export default function Page() {
+  return (
+    <div className="max-w-6xl mx-auto h-[85vh] overflow-hidden">
+      <MessageScreen />
+    </div>
+  );
+}
