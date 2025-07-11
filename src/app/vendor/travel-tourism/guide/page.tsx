3bc4880c9b43a40576ed4bb703f33f3ff3358f3e
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-11 20:07:07 +0530

    feat(vendor:travel-tourism): integrate GuideRequestGrid component into guide page

--- src/app/vendor/travel-tourism/guide/page.tsx
+++ src/app/vendor/travel-tourism/guide/page.tsx
@@ -1,4 +1,5 @@
 import { Testimonials } from "@/components/testimonials";
+import { GuideRequestGrid } from "./components/GuideRequestGrid";
 import { VendorGuidePersonalized } from "./components/VendorGuidePersonalized";
 import { VendorLocalGuideHero } from "./components/VendorLocalGuideHero";
 
@@ -6,7 +7,9 @@ export default function page() {
   return (
     <div>
       <VendorLocalGuideHero />
-      <div className="max-w-6xl mx-auto"></div>
+      <div className="max-w-6xl mx-auto">
+        <GuideRequestGrid />
+      </div>
       <Testimonials />
       <VendorGuidePersonalized />
     </div>
