d205e5efb4122a05fb578fb169d9c95e3e0e5490
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-15 17:35:03 +0530

    fix(auth): adjust layout styles for better responsiveness

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -14,14 +14,14 @@ export default function AuthLayout({
       <main>
         <div className="min-h-[85vh] grid md:grid-cols-2 lg:grid-cols-5">
           {/* Left side - Image and promotional content */}
-          <div className="relative lg:col-span-2">
+          <div className="relative h-[85vh] lg:col-span-2">
             <Suspense fallback={<div>Loading carousel...</div>}>
               <AuthCarousel />
             </Suspense>
           </div>
 
           {/* Right side - Auth forms */}
-          <div className="lg:col-span-3 w-full flex items-center justify-center p-8">
+          <div className="lg:col-span-3 w-full flex items-center justify-center p-5 md:p-8">
             <div className="w-full max-w-xl">{children}</div>
           </div>
         </div>
