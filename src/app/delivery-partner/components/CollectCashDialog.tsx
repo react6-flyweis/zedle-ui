7175883f705318511eb8803eff721ee9bb20539c
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-07-15 11:08:36 +0530

    feat(delivery-partner): add cash icon and localization for CollectCashDialog

--- src/app/delivery-partner/components/CollectCashDialog.tsx
+++ src/app/delivery-partner/components/CollectCashDialog.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { HandCoins } from "lucide-react";
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,6 +9,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import cashIcon from "../assets/cash.gif";
 
 interface CollectCashDialogProps {
   open: boolean;
@@ -21,32 +22,36 @@ export function CollectCashDialog({
   onClose,
   amount = 424.0,
 }: CollectCashDialogProps) {
-  const t = useTranslations("delivery");
+  const t = useTranslations("collectCashDialog");
   return (
     <Dialog open={open} onOpenChange={onClose}>
       <DialogContent className="rounded-2xl p-0 max-w-md">
-        <div className="bg-white rounded-2xl">
-          <DialogHeader className="px-6 pt-6 pb-2">
+        <div className="">
+          <DialogHeader className="px-4 pt-4 pb-2">
             <DialogTitle className="text-2xl font-bold">
-              {t("collectCash.title")}
+              {t("title")}
             </DialogTitle>
-            <p className="text-sm text-muted-foreground mt-2">
-              {t("collectCash.description")}
+            <p className="text-sm text-muted-foreground mt-1">
+              {t("description")}
             </p>
           </DialogHeader>
           <div className="border-b my-2" />
-          <div className="flex flex-col items-center px-6 py-8 gap-6">
+          <div className="flex flex-col items-center p-6 gap-6">
             <span className="text-3xl font-semibold text-primary">
               ${amount.toFixed(2)}
             </span>
-            <div className="flex justify-center items-center">
-              <HandCoins size={80} strokeWidth={1.5} className="text-primary" />
-            </div>
+            <Image
+              src={cashIcon}
+              alt="Cash Icon"
+              width={150}
+              height={150}
+              className=""
+            />
             <Button
               className=" w-full text-lg font-medium rounded-lg h-12 mt-4"
               onClick={onClose}
             >
-              {t("collectCash.done")}
+              {t("done")}
             </Button>
           </div>
         </div>
